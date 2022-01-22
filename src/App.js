4acbe60a349b8577c50fc19e33f5218678c54868
arbab.muhammad <arbab.muhammad@evampsaanga.com>
2022-01-22 15:27:06 +0500

    worked done on the routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 function App() {
-  return <Homepage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
