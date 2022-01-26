870607817c1dd1650c3d9380cab92528ae6f30f4
arbab.muhammad <arbab.muhammad@evampsaanga.com>
2022-01-26 16:48:41 +0500

    worked done on the updated shop data with new data , updated the cart utils to group icons

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { cartActionTypes } from "./cart.types";
-
+import { addItemToCart } from "./cart.utils";
 const initialState = {
   hidden: true,
+  cartItem: [],
 };
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -9,6 +10,11 @@ const cartReducer = (state = initialState, action) => {
       return {
         ...state,
         hidden: !state.hidden,
+      };
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItem: addItemToCart(state.cartItem, action.payload),
       };
     default:
       return state;
